0b845966ef5bcac1b2bbc2f593439e28773cf4f8
Timur Appaev <alloaolla@gmail.com>
2020-03-06 21:49:41 +0300

    exec: reordered code

--- TauExec.hs
+++ TauExec.hs
@@ -8,19 +8,13 @@ import TauParser
 
 exec ast = resolve Map.empty ast
 
-resolve scope (ID name) = case Map.lookup name scope of { Nothing -> ID name; Just x -> x}
+resolve scope cgt@(ID name) = resolve' cgt (Map.lookup name scope)
+    where resolve' cgt (Nothing) = cgt
+          resolve' _ (Just x)  = x
 
 resolve _ func@(FDEF _ _ _) = func
 resolve _ func@(FCOMP _ _)  = func
 resolve scope (APPLY func params) = run (resolve scope func) (map (resolve scope) params)
-
-onlyUniqueIds [] = True
-onlyUniqueIds (x:xs) = (x `notElem` xs) && (onlyUniqueIds xs)
-
-getKeys (FDEF (ARGS args) _ _ )
-  | onlyUniqueIds args = args
-getKeys (FCOMP (ARGS args) _  )
-  | onlyUniqueIds args = args
 
 -- run func params | trace ("\nrun\n\tfunc: " ++ show func ++ "\n\tparams: " ++ show params) False = undefined
 run func@(ID _ ) params = APPLY func params
@@ -31,6 +25,12 @@ run func params
     where isPartialApply keys params = ((ID "_") `elem` params) || ((length params) < (length keys))
           isFullApply    keys params = length params == length keys
           keys = getKeys func
+              where getKeys (FDEF (ARGS args) _ _ ) = getKeys' args
+                    getKeys (FCOMP (ARGS args) _  ) = getKeys' args
+                    getKeys' args
+                      | onlyUniqueIds args = args
+                        where onlyUniqueIds [] = True
+                              onlyUniqueIds (x:xs) = (x `notElem` xs) && (onlyUniqueIds xs)
           scope = makeScope keys params
           apply (FCOMP _ body) scope = resolve scope body
           apply (FDEF (ARGS args) (MATCH id) clauses) scope
@@ -39,24 +39,22 @@ run func params
                     matchValue = resolve scope (ID id)
                     getClause (ID name)                clauses = getClause' name clauses 0
                     getClause (APPLY (ID name) params) clauses = getClause' name clauses (length params)
+                    getClause' name clauses arity = filterClausesByName name $ filterClausesByArity arity clauses
+                        where filterClausesByArity arity clauses = filter ((\arity (CLAUSE _ (ARGS args) _ ) -> (length args) == arity) arity) clauses
+                              filterClausesByName  name  clauses = filter ((\name  (CLAUSE (MATCH x)  _  _ ) -> name == x || x == "_")  name ) clauses
                     resolveClause cgt@(ID _) ([(CLAUSE (MATCH match) _ body)])     = resolve (extendScope scope match [] [] cgt) body
                     resolveClause (APPLY cgt matchValues) ([(CLAUSE (MATCH match) (ARGS matchNames) body)]) = resolve (extendScope scope match matchNames matchValues cgt) body
+                    extendScope scope match keys values cgt
+                        | match == "_"  = Map.union (Map.fromList [("_", cgt)]) extendedScope
+                        | otherwise     = extendedScope
+                        where extendedScope
+                                | (Map.intersection extension scope) ==  Map.empty = Map.union extension scope
+                                    where extension = makeScope keys values
           reducedArgs = filter ((\scope key -> Map.notMember key scope) scope) keys
           reducedFunc = APPLY func resolvedParams
               where resolvedParams = map resolver (zip keys extendedParams) 
                       where extendedParams = params ++ (take ((length keys) - (length params)) (repeat (ID "_")))
                             resolver (key, (ID "_")) = ID key
                             resolver (_, param) = param
-
-getClause' name clauses arity = filterClausesByName name $ filterClausesByArity arity clauses
-    where filterClausesByArity arity clauses = filter ((\arity (CLAUSE _ (ARGS args) _ ) -> (length args) == arity) arity) clauses
-          filterClausesByName  name  clauses = filter ((\name  (CLAUSE (MATCH x)  _  _ ) -> name == x || x == "_")  name ) clauses
-
-extendScope scope match keys values cgt
-    | match == "_"  = Map.union (Map.fromList [("_", cgt)]) extendedScope
-    | otherwise     = extendedScope
-    where extendedScope
-            | (Map.intersection extension scope) ==  Map.empty = Map.union extension scope
-                where extension = makeScope keys values
 
 makeScope keys params = Map.fromList $ filter (\(key, val) -> key /= "_" && val /= ID "_" ) (zip keys params)
